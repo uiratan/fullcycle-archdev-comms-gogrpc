72782f6a431600b2ced05f5479ffeff6f87a0aa6
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-05 11:13:34 -0300

    feat: get category

--- internal/service/category.go
+++ internal/service/category.go
@@ -54,3 +54,18 @@ func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb
 
 	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Find(in.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
